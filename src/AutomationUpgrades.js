64adfd83a3c0ef1e7841e8a0d875e1aee02c75e4
LeadPoisoning <gschwartzenager@gmail.com>
2019-09-11 18:48:04 -0500

    Change style of AutomationUpgrades

--- src/AutomationUpgrades.js
+++ src/AutomationUpgrades.js
@@ -1,56 +1,52 @@
 import React from 'react'
 import {
-          Button,
-          Container,
-          Row,
-          Col,
-          OverlayTrigger
-       } from 'react-bootstrap'
+  Button,
+  Container,
+  Row,
+  Col,
+  OverlayTrigger
+} from 'react-bootstrap'
 
 class AutomationUpgrades extends React.Component {
+  render () {
+    const renderTooltip = () => (
+      <div
+        style={{
+          backgroundColor: 'rgba(0, 0, 0, 0.85)',
+          padding: '2px 10px',
+          color: 'white',
+          borderRadius: 3
+        }}
+      >
+        Costs $1000
+      </div>
+    )
 
-    render() {
-
-        const renderTooltip = props => (
-            <div
-                {...props}
-                style={{
-                backgroundColor: 'rgba(0, 0, 0, 0.85)',
-                padding: '2px 10px',
-                color: 'white',
-                borderRadius: 3,
-                ...props.style,
-                }}
+    return (
+      <Container>
+        <Row className='justify-content-md-center'>
+          <Col sm='2'>
+            <OverlayTrigger
+              placement='right-start'
+              delay={{ show: 250, hide: 400 }}
+              overlay={renderTooltip}
             >
-                Costs $1000
-            </div>
-        );
-
-        return (
-            <Container>
-                <Row className="justify-content-md-center">
-                    <Col sm="2">
-                        <OverlayTrigger
-                            placement="right-start"
-                            delay={{ show: 250, hide: 400 }}
-                            overlay={renderTooltip}
-                        >
-                            <Button variant="primary">
-                            Buy
-                            </Button>
-                        </OverlayTrigger>
-                    </Col>
-                    <Col sm="6">
-                        <Row className="justify-content-md-center">
-                            <p>Math game bot.</p>
-                        </Row>
-                        <Row className="justify-content-md-center">
-                            <p>Costs $1000</p>
-                        </Row>
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
+              <Button variant='primary'>
+              Buy
+              </Button>
+            </OverlayTrigger>
+          </Col>
+          <Col sm='6'>
+            <Row className='justify-content-md-center'>
+              <p>Math game bot.</p>
+            </Row>
+            <Row className='justify-content-md-center'>
+              <p>Costs $1000</p>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
 export default AutomationUpgrades
