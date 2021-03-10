769a0e7f388aaefae2761dddc165ee5aa3f614df
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-10 23:27:17 +0000

    Deploy AndrewCash/addiction-sim to github.com/AndrewCash/addiction-sim.git:gh-pages

---
+++ precache-manifest.0965efd52753b50ef7996abd5668fd33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "269d99a28a2270c5fdd9f5502db98da7",
+    "url": "/addiction-sim/index.html"
+  },
+  {
+    "revision": "db970731e2be1d225fae",
+    "url": "/addiction-sim/static/css/main.6d63c94c.chunk.css"
+  },
+  {
+    "revision": "4971aa3dda0403ed3f83",
+    "url": "/addiction-sim/static/js/2.33d0d75f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/addiction-sim/static/js/2.33d0d75f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db970731e2be1d225fae",
+    "url": "/addiction-sim/static/js/main.1749e0c3.chunk.js"
+  },
+  {
+    "revision": "5e35c791d33df5b2303b",
+    "url": "/addiction-sim/static/js/runtime-main.97706dfd.js"
+  }
+]);
